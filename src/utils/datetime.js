5a99105c1fcdf1f07de914fd29486f392c75212b
archilleu <archilleu9527@gmail.com>
2020-09-17 17:19:53 +0800

    异常处理

--- src/utils/datetime.js
+++ src/utils/datetime.js
@@ -2,9 +2,19 @@ import moment from 'moment'
 
 // element ui 日期通用格式化
 export function formatDate (row, column) {
-  return moment(row[column.property]).format('YYYY-MM-DD')
+  try {
+    return moment(row[column.property]).format('YYYY-MM-DD')
+  } catch (e) {
+    console.error(e)
+  }
+  return null
 }
 
 export function formatDateTime (row, column) {
-  return moment(row[column.property]).format('YYYY-MM-DD hh:mm:ss')
+  try {
+    return moment(row[column.property]).format('YYYY-MM-DD hh:mm:ss')
+  } catch (e) {
+    console.error(e)
+  }
+  return null
 }
